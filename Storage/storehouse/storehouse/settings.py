a06760b361b624a50bec1e9b23e2efb2be2306c7
Konstantin Shchegolev <konstantinshchegolev@Konstantins-MacBook-Air.local>
2021-03-31 14:00:16 +0300

    s

--- Storage/storehouse/storehouse/settings.py
+++ Storage/storehouse/storehouse/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bl=7i-t*3c+(@oc3$ocfa4^pge&egiofv5c^vxg7(@u12f@edc'
+SECRET_KEY = os.environ.get("SECRET_KEY", 'mysecretkeyis...')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'warehouse.apps.WarehouseConfig',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- Storage/storehouse/warehouse/models.py
+++ Storage/storehouse/warehouse/models.py
@@ -1,3 +1,73 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+User = get_user_model()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+
+    class LoanStatus(models.IntegerChoices):
+        AVAILABLE = 1, _('available')
+        NOT_AVAILABLE = 2, _('not available')
+
+    description = models.TextField()
+    title = models.CharField(max_length=255)
+    author = models.ManyToManyField(Author)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='static/images', blank=True)
+    publication_year = models.PositiveSmallIntegerField()
+    status = models.PositiveSmallIntegerField(
+        choices=LoanStatus.choices, default=LoanStatus.AVAILABLE, blank=True
+    )
+    rating = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=10000,
+                               help_text="Input your comment.")
+    rating = models.PositiveSmallIntegerField(
+        choices=(
+            (1, "★☆☆☆☆"),
+            (2, "★★☆☆☆"),
+            (3, "★★★☆☆"),
+            (4, "★★★★☆"),
+            (5, "★★★★★"),
+        )
+
+    )
+
+
+class CartBook(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Cart(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_books = models.PositiveIntegerField(default=0)
+    total_price = models.DecimalField(max_digits=9, decimal_places=2)
+    product = models.ForeignKey(CartBook, on_delete=models.CASCADE)
+
